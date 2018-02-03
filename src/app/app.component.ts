10a6dc8c363df889b73d7fb99411c978bb0e30e5
frkonrad112211 <fryszkowskikonrad@gmail.com>
2018-02-03 01:35:24 +0100

    suggest 75%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,35 +2,35 @@ import {Component} from '@angular/core';
 import {Clinic, Doctor, Registration} from './models';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lat = 51.1078852;
-  lng = 17.0385376;
-  doctors: Doctor[] = [{id: 1, firstname: 'Jan', lastname: 'Kowalski'}];
-  clinics: Clinic[] = [
-    {
-      id: 1,
-      name: 'Scanmed',
-      logo: 'assets/icons/scanmed.png',
-      address: 'Rynek 3',
-      latitude: 51.1078852,
-      longitude: 17.0385376,
-      doctors: this.doctors
-    },
-    {
-      id: 2,
-      name: 'Klinika 2',
-      logo: 'assets/icons/scanmed.png',
-      address: 'Rynek 3',
-      latitude: 51.11276390719499,
-      longitude: 17.018980979919434,
-      doctors: this.doctors
-    }
-  ];
-  registrations: Registration[] = [
-    {id: 1, clinic: this.clinics[0], doctor: this.doctors[0], date: new Date()},
-    {id: 2, clinic: this.clinics[1], doctor: this.doctors[0], date: new Date()}];
+    lat = 51.1078852;
+    lng = 17.0385376;
+    doctors: Doctor[] = [{id: 1, firstname: 'Jan', lastname: 'Kowalski'}];
+    clinics: Clinic[] = [
+        {
+            id: 1,
+            name: 'Scanmed',
+            logo: 'assets/icons/scanmed.png',
+            address: 'Rynek 3',
+            latitude: 51.1078852,
+            longitude: 17.0385376,
+            doctors: this.doctors
+        },
+        {
+            id: 2,
+            name: 'Klinika 2',
+            logo: 'assets/icons/scanmed.png',
+            address: 'Rynek 3',
+            latitude: 51.11276390719499,
+            longitude: 17.018980979919434,
+            doctors: this.doctors
+        }
+    ];
+    registrations: Registration[] = [
+        {id: 1, clinic: this.clinics[0], doctor: this.doctors[0], date: new Date()},
+        {id: 2, clinic: this.clinics[1], doctor: this.doctors[0], date: new Date()}];
 }
--- src/app/suggest-window/suggest-window.component.ts
+++ src/app/suggest-window/suggest-window.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {Clinic} from '../models';
+import {Clinic, Doctor, Registration} from '../models';
+
+import {DialogService} from 'ng2-bootstrap-modal';
+import {RegisterFormComponent} from '../register-form/register-form.component';
 
 @Component({
   selector: 'app-suggest-window',
@@ -10,20 +13,47 @@ export class SuggestWindowComponent implements OnInit {
   @Input() lat: number;
   @Input() lng: number;
   @Input() clinics: Array<Clinic>;
-  clinicName = "Przychodnia Ziółko";
-  clinicStreet = "Łączności 13/1, Wrocław";
-  hour = "15:00";
-  doctorName = "Dr. Konrad Fryszkowski";
-  constructor() {
+  @Input() registrations: Array<Registration>;
+  clinicFound:string;
+  clinicName:string;
+  clinicStreet:string;
+  doctorName:string;
+  hour:number;
+  minutes:number;
+  day:number;
+  month:number;
+  year:number;
 
+  constructor(private dialogService: DialogService) {
   }
 
   ngOnInit() {
+    this.clinicFound = "We have found clinic for you";
+    this.clinicName = this.clinics[0].name;
+    this.clinicStreet = this.clinics[0].address;
+    this.doctorName = this.clinics[0].doctors[0].firstname +" "+ this.clinics[0].doctors[0].lastname;
+    this.hour = this.registrations[0].date.getUTCHours();
+    this.minutes = this.registrations[0].date.getUTCMinutes();
+    this.day = this.registrations[0].date.getUTCDay();
+    this.month = this.registrations[0].date.getUTCMonth();
+    this.year = this.registrations[0].date.getUTCFullYear();
+
     if(this.lat==0 || this.lng == 0){
-       this.clinicStreet =this.hour = this.doctorName = "";
+       this.clinicStreet  = this.doctorName = "";
        this.clinicName = "Ni ma lokacji"
     }
 
+  }
+
+  showConfirm(registration: Registration) {
+    console.log('test');
+    this.dialogService.addDialog(RegisterFormComponent, {
+      registration: this.registrations[0]
+    });
+  }
+
+  moveToDoctor(clinic: Clinic) {
+    console.log(clinic);
   }
 
 }
